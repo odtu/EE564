b462d9e83299287688e746332e00e79c195b1db5
hyuruk <hyuruk@gmail.com>
2016-04-09 18:12:32 +0300

    core selection, cupper losses

---
+++ XRAY/hyuruk/xray_code_core_selection_hy.m
@@ -0,0 +1,35 @@
+% In this code, it is supposed to design a high-frequency, 
+% high-voltage transformer that will be used in a X-Ray device.
+%-----------------------------------------------------------------------  
+% Huseyin YURUK
+%----------------------------------------
+% Following design guide is used:
+% Magnetics Ferrite Power Design 2013
+%----------------------------------------
+% Core Selection by WaAc product
+% The power handling capacity of a transformer core can also be determined 
+% by its
+% WaAc product, where Wa is the available core window area, and Ac is 
+% the effective
+% core cross-sectional area. 
+% Area Product Distribution (WaAc)
+% WaAc = (Po * Dcma) / (Kt * Bmax * f)
+% where
+% WaAc = Product of window area and core area (cm4)
+% Po = Power Out (watts)
+% Dcma = Current Density (cir. mils/amp)
+% Bmax = Flux Density (gauss)
+% ƒ = frequency (hertz)
+% Kt = Topology constant  (Full-bridge = 0.0014)
+Po = 30 * 10^3;     % input parameter
+f = 100 * 10^3;     % input parameter
+Kt = 0.0014;        
+% for cir. mils to mm^2 see below link
+% conversion see http://www.convertunits.com/from/mm%5E2/to/circular+mil
+% 1mm^2 ~1973.5 cir. mils
+J = 2.5;            % current density A/mm^2
+Dcma = 1973.5 / J; 
+Bmax = 0.47 * 10^4; % P type core has the 0.47T max. flux density
+WaAc = Po * Dcma / (Kt * Bmax * f)
+
+%%%%%%
---
+++ XRAY/hyuruk/xray_code_window_utilization_cable_hy.m
@@ -0,0 +1,36 @@
+% Approximately primary and secondary wire size are can be calculated
+% as follows:
+% Ku * Wa = Np*Awp + Ns*Aws
+% Ku is fill factor
+% Ku = s1*s2*s3*s4
+% s1: wire isulation,  conductor area/wire area 
+% s2: fill factor,  wound area/usable window area
+% s3: effective window, usable window area/window area
+% s4: insulation factor, sable window area/usable window area + insulation  
+% Note that at 100Khz to minimize skin effect AWG26 is used
+% for more details see below link
+% http://coefs.uncc.edu/mnoras/files/2013/03/
+% Transformer-and-Inductor-Design-Handbook_Chapter_4.pdf
+% assume
+% Np*Awp = 1.1 * Ns*Aws (to allow for losses)
+
+% for AWG26 @100kHz
+s1 = 0.79;             
+s2 = 0.61;
+s3 = 0.6;
+s4 = 1;
+Ku = s1*s2*s3*s4;
+
+D = 31.7;             % core dimensions in mm
+E = 50.8;             % core dimensions in mm
+Wa = 2 * D*E * 10^-2; % available core window area cm^2
+Aws = Ku * Wa * 10^2 / (2.1 * Ns);   % mm^2
+Awp = 1.1 * Ns * Aws / Np;          % mm^2
+    
+% mm^2   %requirred wire size for choosen current density 
+Awp_req = Ip / J;      
+Aws_req = Is / J;      
+
+%%%%%%
+
+
