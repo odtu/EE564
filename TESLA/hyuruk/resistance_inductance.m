2cfb884eaacac04623f57e464da95966bb166689
Kaan Mutlu <mehmet.kaan.mutlu@gmail.com>
2016-05-17 03:18:49 +0300

    no message

--- TESLA/hyuruk/resistance_inductance.m
+++ TESLA/hyuruk/resistance_inductance.m
@@ -13,8 +13,18 @@ Lec = 2*u0*L*N_per_ph_req^2/(p1*q)*lambdaec; % [H] end winding inductance
                                              % derived from (eq. 15.75)
 ler = pi*(Dis-(2*g*10^-3)-(3*10^-3)-b)/Nr;   % [m] end ring segment
                                              % (eq. 15.71 & Dre-Der~=3-4mm
-pal_80deg = 3.1*10^-8*(1+(80-20)/273);       % [ohm.m] aluminium resistivity
+pal_20deg = 3.1*10^-8;                                             
+pal_80deg = pal_20deg*(1+(80-20)/273);       % [ohm.m] aluminium resistivity
 Rer = pal_80deg*(ler/(2*Aer*sin(pi*p1/Nr)^2)); % [ohm] rotor end ring segment
                                                % derived from (eq. 15.70)
-
+mbetas = sqrt((2*pi*f1*u0)/(2*pal_20deg));     % [m^-1] (eq. 15.73)
+S = 1;
+meps = mbetas*hr*sqrt(S);                      % [-] (eq. 15.73)
+Kr = meps;                                     % [-] (eq. 15.72)         
+Rbes1 = pal_80deg*(L/Ab)*Kr+Rer; % [ohm] rotor bar/end ring segment 
+                               % eq. circuit (eq. 15.70)
+Rrps1 = (4*m/Nr)*(N_per_ph_req*Kw1)^2*Rbes1; % [ohm] rator cage resistance 
+                               % referred to stator at S=1 (eq. 15.74)
+Rbesn = pal_80deg*(L/Ab)+Rer;  % [ohm] for rated slip 
+Rrpsn = Rrps1*(Rbesn/Rbes1);   % [ohm] (eq. 15.87)
                                                  
